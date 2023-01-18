bde1a4316de5414af0eec4734c31d4ef2f89d090
OdeToCode147 <109613172+OdeToCode147@users.noreply.github.com>
2023-01-18 20:42:18 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const mainRouter = express.Router();
 
 mainRouter.get('/latest',homeData.getLatestData)
 mainRouter.get('/latestArticle',homeData.getLatestArticleData)
+mainRouter.get('/moreFromSiren',homeData.getMorefromSirenData)
 
 module.exports = mainRouter
